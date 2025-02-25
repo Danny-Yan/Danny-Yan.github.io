b559f7fec867224754b96aaa9fda894ceba8694b
Danny Yan <yandanny2004@gmail.com>
2025-02-26 01:01:42 +1100

    Added desmos display version 2

--- webApp/src/app/components/desmos-components/desmos-display-version-2/desmos-display-version-2.component.ts
+++ webApp/src/app/components/desmos-components/desmos-display-version-2/desmos-display-version-2.component.ts
@@ -1,12 +1,11 @@
 import { Component, Input, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { SvgObjectComponent } from '../../ui/svg-object/svg-object.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons'; 
-import { ButtonLinkComponent } from '../../ui/button-link/button-link.component';
+import { faXmark, faArrowRight } from '@fortawesome/free-solid-svg-icons'; 
 
 @Component({
   selector: 'app-desmos-display-version-2',
-  imports: [SvgObjectComponent, FontAwesomeModule, ButtonLinkComponent],
+  imports: [SvgObjectComponent, FontAwesomeModule],
   templateUrl: './desmos-display-version-2.component.html',
   styleUrl: './desmos-display-version-2.component.css'
 })
@@ -15,8 +14,9 @@ export class DesmosDisplayVersion2Component implements AfterViewInit {
   @Input({required: true}) description: string = '';
   @Input({required: true}) svg: string = '';
   @ViewChild('display_dom') displayDom: ElementRef = ElementRef.prototype;
-  @ViewChild('bg_dom') bgDom: ElementRef = ElementRef.prototype;
+  @ViewChild('desmos_link') desmosLink: ElementRef = ElementRef.prototype;
   faXmark = faXmark;
+  faArrowRight = faArrowRight;
 
   ngAfterViewInit(): void {
       // console.log(this.displayDom.nativeElement);
@@ -24,10 +24,11 @@ export class DesmosDisplayVersion2Component implements AfterViewInit {
 
   open() {
     this.displayDom.nativeElement.style.top = "0px";
-    this.bgDom.nativeElement.style.left = '100%';
+    this.displayDom.nativeElement.classList.add('no-doc-scroll');
   };
   close() {
     this.displayDom.nativeElement.style.top = "-100%";
-    this.bgDom.nativeElement.style.left = '0px';
+    this.displayDom.nativeElement.classList.remove('no-doc-scroll');
+
   };
 }
