3ce918ae14473b2c1e813a404859302d85c62814
Danny Yan <yandanny2004@gmail.com>
2025-03-02 19:32:15 +1100

    fixed mobile UI for desmos page

--- webApp/src/app/web-pages/discord-page/discord-page.component.ts
+++ webApp/src/app/web-pages/discord-page/discord-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { DiscordColumnSingleComponent } from '../../components/discord-components/column-single-wide/discord-column-single/discord-column-single.component';
+import { AppSettingsService } from '../../services/app-settings/app-settings.service';
+import { DiscordColumnTripleComponent } from '../../components/discord-components/column-triple-wide/discord-column-triple/discord-column-triple.component';
 
 @Component({
   selector: 'app-discord-page',
-  imports: [],
+  imports: [DiscordColumnSingleComponent, DiscordColumnTripleComponent],
   templateUrl: './discord-page.component.html',
   styleUrl: './discord-page.component.css'
 })
-export class DiscordPageComponent {
+export class DiscordPageComponent implements OnInit{
+  messages: Array<any> = [];
+  innerWidth: number = 0;
 
+  constructor( private appSettingsService: AppSettingsService) {}
+
+  ngOnInit(){
+    this.appSettingsService.getJSON("test_json.html").subscribe(data => {
+      this.messages = data;
+    });
+    this.innerWidth = window.innerWidth;
+  };
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
 }
