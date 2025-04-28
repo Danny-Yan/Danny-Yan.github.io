dadeb7eb1fbcab5cf4498b8b996938e1cbd7f163
Danny Yan <dyan0038@student.monash.edu>
2025-04-28 13:08:09 +1000

    Added contact page

--- webApp/src/app/web-pages/about-page/about-page.component.ts
+++ webApp/src/app/web-pages/about-page/about-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FrontPageContactComponent } from '../../components/home-components/front-page-contact/front-page-contact.component';
 
 @Component({
   selector: 'app-about-page',
-  imports: [],
+  imports: [FrontPageContactComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
 })
